2b2e30235738b0dcb584fa880490827ce4e79282
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2023-10-17 10:37:09 +0200

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,22 +1,54 @@
 #!/usr/bin/python3
 
-class square():
+class Square:
+	"""
+	A class representing a square shape.
+
+	Attributes:
+		width (int): The width of the square.
+		height (int): The height of the square.
+	"""
+
 	def __init__(self, width=0, height=0):
+		"""
+		Initializes a new instance of the Square class.
+
+		Args:
+			width (int): The width of the square (default is 0).
+			height (int): The height of the square (default is 0).
+		"""
 		self.width = width
 		self.height = height
 
 	def area_of_my_square(self):
-		""" Area of the square """
+		"""
+		Calculates the area of the square.
+
+		Returns:
+			int: The area of the square.
+		"""
 		return self.width * self.height
 
-	def PerimeterOfMySquare(self):
+	def perimeter_of_my_square(self):
+		"""
+		Calculates the perimeter of the square.
+
+		Returns:
+			int: The perimeter of the square.
+		"""
 		return (self.width * 2) + (self.height * 2)
 
 	def __str__(self):
+		"""
+		Returns a string representation of the square.
+
+		Returns:
+			str: The string representation of the square in the format "width/height".
+		"""
 		return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-	s = square(width=12, height=9)
+	s = Square(width=12, height=9)
 	print(s)
 	print(s.area_of_my_square())
-	print(s.PerimeterOfMySquare())
+	print(s.perimeter_of_my_square())
