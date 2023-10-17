5be263a4c416f6feb8622410708f054a1027e940
Youssef <youssefkaramedu@gmail.com>
2023-10-17 01:18:34 -0700

    Add file edits

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -5,7 +5,6 @@ from flask import jsonify
 
 from api.v1.views import app_views
 
-
 @app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of the web server
