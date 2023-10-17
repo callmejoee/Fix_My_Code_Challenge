7297de4372ea899dd4fc215540dbce195079af4d
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2023-10-17 10:43:15 +0200

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,19 +3,19 @@
 class Square:
     """
     A class representing a square shape.
-    
+
     Attributes:
-    - width: The width of the square.
-    - height: The height of the square.
+        width (int): The width of the square.
+        height (int): The height of the square.
     """
 
     def __init__(self, width=0, height=0):
         """
-        Initializes a square with the given width and height.
-        
+        Initializes a new instance of the Square class.
+
         Args:
-        - width: The width of the square.
-        - height: The height of the square.
+            width (int): The width of the square (default is 0).
+            height (int): The height of the square (default is 0).
         """
         self.width = width
         self.height = height
@@ -23,27 +23,27 @@ class Square:
     def area_of_my_square(self):
         """
         Calculates the area of the square.
-        
+
         Returns:
-        The area of the square.
+            int: The area of the square.
         """
         return self.width * self.height
 
     def perimeter_of_my_square(self):
         """
         Calculates the perimeter of the square.
-        
+
         Returns:
-        The perimeter of the square.
+            int: The perimeter of the square.
         """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """
         Returns a string representation of the square.
-        
+
         Returns:
-        A string representation of the square in the format 'width/height'.
+            str: The string representation of the square in the format "width/height".
         """
         return "{}/{}".format(self.width, self.height)
 
